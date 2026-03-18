e3732246f6b9ca0bb0796d4e5da8a2a110b1f641
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2026-03-18 19:47:16 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,41 @@
+# ================================
+# ACHEI! Senha: 443929292
+# Tempo Serial (T=1): 408.5372 segundos
+# Tentativas: 443929292
+# ================================
+
+import hashlib
+import time
+
+hash_alvo = "ca6ae33116b93e57b87810a27296fc36"
+inicio = time.time()
+
+print("---------------------------------")
+print("INICIANDO BUSCA SERIAL (T=1)...")
+print("Se o programa estiver rodando, você verá mensagens abaixo.")
+print("---------------------------------")
+
+# LOOP DIRETO (0 a 999.999.999)
+for i in range(1000000000):
+    
+    # Gera o número com 9 dígitos (ex: 000000001)
+    tentativa = f"{i:09}"
+    
+    # Calcula o hash MD5
+    hash_gerado = hashlib.md5(tentativa.encode('utf-8')).hexdigest()
+    
+    # Verifica se encontrou o hash
+    if hash_gerado == hash_alvo:
+        fim = time.time()
+        tempo_total = fim - inicio
+        
+        print("\n================================")
+        print(f"ACHEI! Senha: {tentativa}")
+        print(f"Tempo Serial (T=1): {tempo_total:.4f} segundos")
+        print(f"Tentativas: {i}")
+        print("================================")
+        break
+
+
+    if i % 10000000 == 0 and i > 0:
+        print(f"Verificando... {i // 1000000} milhões de tentativas feitas.")
